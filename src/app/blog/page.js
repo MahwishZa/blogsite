66301fcab9481d00771717548b560ed81c84a813
MahwishZa <154221769+MahwishZa@users.noreply.github.com>
2025-09-03 17:02:40 +0500

    Update page.js

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,7 +1,12 @@
 import BlogPostCard from '@/components/blogcard'
+import { headers } from 'next/headers'
 
 async function getPosts() {
-  const res = await fetch('http://localhost:3000/api/posts', { cache: 'no-store' }) // Or use your deployed URL
+  const hdrs = headers()
+  const host = hdrs.get('host') || 'localhost:3000'
+  const protocol = process.env.VERCEL ? 'https' : 'http'
+  const base = `${protocol}://${host}`
+  const res = await fetch(`${base}/api/posts`, { cache: 'no-store' })
   if (!res.ok) return []
   return await res.json()
 }
